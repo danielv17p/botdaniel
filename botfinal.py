eb87accec6d4eb2de10a397fdd3e9896d9dd99d9
danielv17p <danielv17p@gmail.com>
2025-01-11 12:32:35 -0600

    Add files via upload

---
+++ botfinal.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.zuser}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def mem(ctx):
+    with open('imagenes/ardilla.jpeg', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
+
+@bot.command()
+async def memuno(ctx):
+    with open ("imagenes/holados.jpeg","rb") as f :
+        picture = discord.File(f)
+        print(os.listdir("imagenes"))
+    await ctx.send(file=picture)
+
+bot.run("token")
+
+
+
